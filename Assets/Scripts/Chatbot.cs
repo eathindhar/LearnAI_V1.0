ccc9f75ea231d32bc5e88b512388a71cc615a54b
Eathindhar Mahendradevan <cldveathi@gmail.com>
2018-03-23 01:03:35 +0530

    First Commit

---
+++ Assets/Scripts/Chatbot.cs
@@ -0,0 +1,145 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Collections;
+using System.Reflection;
+using ApiAiSDK;
+using ApiAiSDK.Model;
+using ApiAiSDK.Unity;
+using Newtonsoft.Json;
+using System.Net;
+using System.Collections.Generic;
+
+public class Chatbot : MonoBehaviour
+{
+
+    public Text answerTextField;
+    public Text inputTextField;
+    public Text responseTextField;
+    private ApiAiUnity apiAiUnity;
+    private AudioSource aud;
+
+    private readonly JsonSerializerSettings jsonSettings = new JsonSerializerSettings
+    {
+        NullValueHandling = NullValueHandling.Ignore,
+    };
+
+    private readonly Queue<Action> ExecuteOnMainThread = new Queue<Action>();
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        // check access to the Microphone
+        yield return Application.RequestUserAuthorization(UserAuthorization.Microphone);
+        if (!Application.HasUserAuthorization(UserAuthorization.Microphone))
+        {
+            throw new NotSupportedException("Microphone using not authorized");
+        }
+
+        ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) =>
+        {
+            return true;
+        };
+
+        const string ACCESS_TOKEN = "2c890cf705f14c859fcdb438300d88eb";
+
+        var config = new AIConfiguration(ACCESS_TOKEN, SupportedLanguage.English);
+
+        apiAiUnity = new ApiAiUnity();
+        apiAiUnity.Initialize(config);
+
+        apiAiUnity.OnError += HandleOnError;
+        apiAiUnity.OnResult += HandleOnResult;
+    }
+
+    void HandleOnResult(object sender, AIResponseEventArgs e)
+    {
+        RunInMainThread(() => {
+            var aiResponse = e.Response;
+            if (aiResponse != null)
+            {
+                Debug.Log(aiResponse.Result.ResolvedQuery);
+                var outText = JsonConvert.SerializeObject(aiResponse, jsonSettings);
+
+                Debug.Log(outText);
+
+                answerTextField.text = aiResponse.Result.ResolvedQuery;
+                responseTextField.text = aiResponse.Result.Fulfillment.Speech;
+                responseTextField.text = aiResponse.Result.Fulfillment.Speech;
+
+            }
+            else
+            {
+                Debug.LogError("Response is null");
+            }
+        });
+    }
+
+    void HandleOnError(object sender, AIErrorEventArgs e)
+    {
+        RunInMainThread(() => {
+            Debug.LogException(e.Exception);
+            Debug.Log(e.ToString());
+            answerTextField.text = e.Exception.Message;
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (apiAiUnity != null)
+        {
+            apiAiUnity.Update();
+        }
+
+        // dispatch stuff on main thread
+        while (ExecuteOnMainThread.Count > 0)
+        {
+            ExecuteOnMainThread.Dequeue().Invoke();
+        }
+    }
+
+    private void RunInMainThread(Action action)
+    {
+        ExecuteOnMainThread.Enqueue(action);
+    }
+
+    public void SendText()
+    {
+        var text = inputTextField.text;
+
+        Debug.Log(text);
+
+        AIResponse response = apiAiUnity.TextRequest(text);
+
+        if (response != null)
+        {
+            Debug.Log("Resolved query: " + response.Result.ResolvedQuery);
+            var outText = JsonConvert.SerializeObject(response, jsonSettings);
+
+            Debug.Log("Result: " + outText);
+
+            answerTextField.text = response.Result.ResolvedQuery;
+            responseTextField.text = response.Result.Fulfillment.Speech;
+        }
+        else
+        {
+            Debug.LogError("Response is null");
+        }
+
+    }
+
+    public void StartNativeRecognition()
+    {
+        try
+        {
+            apiAiUnity.StartNativeRecognition();
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+}
